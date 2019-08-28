a0b545ecbfa28df13d8fc1a573ec1dcc970d4dfd
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-28 14:29:24 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
 
+ attr_accessor :total, :discount, :last_trans
+
+  def initialize(discount = 0 )
+    @total = 0
+    @discount = discount 
+    @last
+  end
+ 
+ def add_item(title, price)
+
+
+end
