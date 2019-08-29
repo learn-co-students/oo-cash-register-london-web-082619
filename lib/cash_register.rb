c1a45a3608bc1de0ce445e1d10c0451f0cd305cd
ck2e14 <chriswkennedy@icloud.com>
2019-08-29 13:29:47 +0100

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,14 +13,20 @@ class CashRegister
         quantity.times do 
           items << title
         end
+        self.last_transaction = price * quantity
     end
 
     def apply_discount
        if discount > 0 
-        "After the discount, the total comes to "
+        self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+        "After the discount, the total comes to $#{self.total}."
         else 
             "There is no discount to apply."
         end
+    end
+
+    def void_last_transaction
+        self.total = self.total - self.last_transaction
     end
 
 
